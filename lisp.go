4b06e68ee8957ad67b6be24497d81b76e963a00b
Conrad Owen <teknobo@gmail.com>
2013-01-08 00:09:45 -0500

    Infinite argument arithmetic

--- lisp.go
+++ lisp.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Env struct {
-  symbols map[string](func(x,y int) int)
+  symbols map[string](func(_ []int) int)
   outerEnv *Env
 }
 
@@ -27,25 +27,49 @@ func (env Env) find(v string) Env {
   return Env{}
 }
 
+func reduceInts(init int, f (func(_,_ int) int), nums []int) int {
+  result := init
+  for _, num := range nums {
+    result = f(result, num)
+  }
+  return result
+}
+
 func add_globals(env *Env) {
   if env.symbols == nil {
-    env.symbols = make(map[string](func(x,y int) int))
+    env.symbols = make(map[string](func(_ []int) int))
   }
 
-  env.symbols["+"] = func(x,y int) int {
-    return x + y
+  env.symbols["+"] = func(nums []int) int {
+    sum := 0
+    for _, num := range nums {
+      sum += num
+    }
+    return sum
   }
 
-  env.symbols["-"] = func(x,y int) int {
-    return x - y
+  env.symbols["-"] = func(nums []int) int {
+    diff := nums[0]
+    for _, num := range nums[1:] {
+      diff -= num
+    }
+    return diff
   }
 
-  env.symbols["*"] = func(x,y int) int {
-    return x * y
+  env.symbols["*"] = func(nums []int) int {
+    prod := nums[0]
+    for _, num := range nums[1:] {
+      prod = prod * num
+    }
+    return prod
   }
 
-  env.symbols["/"] = func(x,y int) int {
-    return x / y
+  env.symbols["/"] = func(nums []int) int {
+    quot := nums[0]
+    for _, num := range nums[1:] {
+      quot /= num
+    }
+    return quot
   }
 }
 
@@ -90,11 +114,16 @@ func eval(exp []string, env *Env) int {
   // fmt.Println("env has: ",env)
   // fmt.Println("We're looking for",exp[0])
   envValue := env.find(exp[0])
-  arg1, _ := atomize(exp[1])
-  arg2, _ := atomize(exp[2])
+  args := make([]int, len(exp[1:])-1)
+  for i, exp := range exp[1:] {
+    if ")" == exp {
+      break
+    }
+    args[i], _ = atomize(exp)
+  }
   // fmt.Println("Eval func: <", exp[0], "> with args: ",arg1,arg2)
   // fmt.Println("We return: ", envValue.symbols[exp[0]](arg1, arg2))
-  return envValue.symbols[exp[0]](arg1, arg2)
+  return envValue.symbols[exp[0]](args)
 }
 
 func main() {
